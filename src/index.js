9a8c775de05a11f641d44b94b71f61beafc3b884
Nate Judice <nate_judice@stuller.com>
2022-06-01 16:02:27 -0500

    use life production server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://naj64-server-castronauts.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
